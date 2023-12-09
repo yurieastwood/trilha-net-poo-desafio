918a310857c09fba9a23259b108c2b803f342bf7
Yuri Eastwood <yurieastwood@users.noreply.github.com>
2023-12-08 20:03:28 -0600

    Resolvendo TODOs

--- Program.cs
+++ Program.cs
@@ -1,3 +1,21 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+namespace DesafioPOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Smartphone Nokia:");
+            var nokia = new Nokia("123456789", "Nokia 3310", "123456789", 16);
+            nokia.Ligar();
+            nokia.InstalarAplicativo("Snake");
+
+            Console.WriteLine("Smartphone iPhone:");
+            var iphone = new Iphone("987654321", "Iphone 11", "987654321", 64);
+            iphone.ReceberLigacao();
+            iphone.InstalarAplicativo("Instagram");
+        }
+    }
+}
